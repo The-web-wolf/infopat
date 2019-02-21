3068f0bb3f26b3134e4919614954048841969072
Emmnauel Abeshi <abeshiemmanuel37@gmail.com>
2019-02-21 14:49:08 +0100

    update

--- newsw.js
+++ newsw.js
@@ -2,7 +2,7 @@
 
 //Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', function(event) {
-  var indexPage = new Request('/');
+  var indexPage = new Request('/infopat');
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
